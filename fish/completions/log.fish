24a0ec8e1f1c8948fcc761bf99df7643459e5f04
Andreas Broström <andreas.brostrom@gmail.com>
2016-02-25 11:47:12 +0100

    Add completiong for log and update. Fixes #8

---
+++ fish/completions/log.fish
@@ -0,0 +1,3 @@
+complete -c log -l warning -d "Log warning text"
+complete -c log -l error -d "Log error text"
+complete -c log -f -a message -d "Logs 'message'"
---
+++ fish/completions/update.fish
@@ -0,0 +1,3 @@
+complete -c update -l no-brew -d "Do not upgrade brew"
+complete -c update -l softwareupdate -d "Run softwareupdate"
+complete -c update -l node-lts -d "Update node to latest LTS"
